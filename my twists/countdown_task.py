43d20859b5adfcb454829254818c80845cec393c
iris <martinmshale@gmail.com>
2016-08-04 11:56:01 +0300

    LoopCallback implementation of twisted.internet.task

--- my twists/countdown_task.py
+++ my twists/countdown_task.py
@@ -2,7 +2,12 @@ from twisted.internet.task import LoopingCall
 from twisted.internet import reactor
 
 class Countdown(object):
- 
+ """
+	Consider the LoopingCall class in twisted.internet.task.
+	Rewrite the countdown program above to use LoopingCall. 
+	You only need the start and stop methods and you don’t need to use the “deferred” return value
+	 in any way
+ """
     counter = 5
  
     def count(self):
